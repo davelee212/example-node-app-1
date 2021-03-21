b5c1fd8d08b667e9a1d57f44963f958e1d470854
Dave Lee <davelee212>
2021-03-21 11:20:00 +0000

    added some extra pages

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     moment = require("moment")
 
-    
 const port = process.env.PORT || 3000
 
 moment.locale("en-gb");
@@ -17,6 +16,17 @@ app.listen(port, function() {
     console.log("Server has started on port: " + port)
 })
 
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/profile", function(req, res) {
+    res.render("profile");
+});
+
+app.get("/signup", function(req, res) {
+    res.render("signup");
+});
 
 app.get("/", function(req, res) {
     res.render("landing");
